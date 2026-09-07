90bb2864fe3a15495a878eb72beb435ae0e3bd0b
jperona <jeromeperona@yahoo.com>
2026-09-07 09:56:49 +0800

    feat: implement SkipsEmptyRows interface in SupplierImport for improved data handling

--- app/Imports/SupplierImport.php
+++ app/Imports/SupplierImport.php
@@ -3,10 +3,11 @@
 namespace App\Imports;
 
 use App\Models\Supplier;
+use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class SupplierImport implements ToModel, WithHeadingRow
+class SupplierImport implements ToModel, WithHeadingRow, SkipsEmptyRows
 {
     public function model(array $row)
     {
